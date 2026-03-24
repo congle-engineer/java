27b05fc8ca4b6521498eeb879284cf1e21539e90
congle-engineer <congle.engineer@gmail.com>
2026-03-24 22:54:21 +0700

    Build Hello World with cc

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    id("java")
+    application
+}
+
+application {
+    mainClass.set("com.example.App")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
